b6fbe9196e7506a6aa1918038664ed361f9daaf2
Sean O'Connor <seanie.beg@gmail.com>
2016-03-10 21:53:09 +0000

    Comments section added

--- funTimesApp/app.js
+++ funTimesApp/app.js
@@ -11,6 +11,10 @@ var funtimesapp = angular.module('funTimesApp', ['ngRoute'])
             templateUrl: 'partials/about.html',
             controller: 'aboutFunTimes'
           })
+          .when('/games/:gameId', {
+            templateUrl: 'partials/game-reviews.html',
+            controller: 'GameReviewCtrl'
+          })
           .otherwise({
             redirectTo: '/homepage'
           })
@@ -23,8 +27,41 @@ var funtimesapp = angular.module('funTimesApp', ['ngRoute'])
                  })
              $scope.orderProp = 'age';
      }])
+/*
+     funtimesapp.controller('GameReviewCtrl', ['$scope',
+       'GameService', 
+       '$routeParams',
+       function ($scope,GameService ,$routeParams) {
+             $scope.games = GameService.getGames($routeParams.game)
+      }])
+*/
+      funtimesapp.controller('GameReviewCtrl', 
+         ['$scope', '$location', '$routeParams', 'GameService', 
+         function($scope, $location, $routeParams, GameService) {
+             GameService.getGame($routeParams.gameId)
+                .success(function(data) {
+                   $scope.game = data
+                   //$scope.comments = $scope.game.comments[0]
+                   //$scope.img = $scope.phone.images[0]
+                   })
+                .error(function(err) {
+                    $location.path('./homepage-items') 
+                  })
+            $scope.incrementUpvotes = function(comment) {
+                  comment.upvotes += 1;
+            }
+            $scope.addComment = function(){
+	            $scope.game.comments.push({
+                    "body": $scope.comment.body,
+                    "author": $scope.comment.author,
+                    "upvotes": 0
+                });
+	            //$scope.comment = {} ;
+          	}
 
-        funtimesapp.controller('aboutFunTimes', ['$scope', function($scope) {
+      }])
+
+      funtimesapp.controller('aboutFunTimes', ['$scope', function($scope) {
     }]) 
 
      funtimesapp.factory('GameService', ['$http' , function($http){
@@ -32,9 +69,9 @@ var funtimesapp = angular.module('funTimesApp', ['ngRoute'])
                 getGames : function() {
                     return $http.get('games/games.json')            
                 }, 
-               // getPhone : function(id) {  // NEW
-                //     return $http.get('games/' + id + '.json')
-                //}
+               getGame : function(id) {  // NEW
+                    return $http.get('games/' + id + '.json')
+                }
             }
             return api
         }])
